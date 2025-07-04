bd0d234f8db204f41d959e1198f797e99ed56090
Randy <raoulrandy3@gmail.com>
2025-07-04 19:30:47 +0300

    ADD:footer and partnaire section

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './css/index.css'
+import Footer from './components/footer.jsx'
+import Partenaire from './components/partenaire.jsx'
+function App (){
+    return (
+        <>
+            <Partenaire />
+            <Footer />
+        </>
+    )
+}
+export default App
